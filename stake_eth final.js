eceb1f5d053aac7455360677e4ea5f7d694017ee
Scorpio8433 <159675123+Scorpio8433@users.noreply.github.com>
2025-03-17 19:59:26 -0500

    Update stake_eth final.js

--- stake_eth final.js
+++ stake_eth final.js
@@ -1,12 +1,8 @@
 const { ExternalAddress, Coinbase, StakeOptionsMode } = require('coinbase-sdk'); // Make sure to install the Coinbase SDK if you haven't already
 
 // Set your API key and secret
-const API_KEY = '382c5e1a-c509-41b2-a168-cc95097731d6';
-const API_SECRET = `-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEINabO5xnjazAdxwl72PhBJhC9qdlAbnhJG1yhDULPX0QoAoGCCqGSM49
-AwEHoUQDQgAEwjaLljvTQR6/H3eTfeoRUHHtFEtmTlj6jD9F+ueo5qrJh4wPkDaT
-mdyaXTt4kOfiCm5LFs+4Vf50deW5zT6YNw==
------END EC PRIVATE KEY-----`;
+const API_KEY = 'YOUR_API_KEY_HERE';
+const API_SECRET = 'YOUR_API_SECRET_HERE';
 
 // Configure Coinbase SDK with your API key and secret
 Coinbase.configure({
@@ -17,7 +13,7 @@ Coinbase.configure({
 async function stakeEthereum() {
     try {
         // Create an ExternalAddress object for Ethereum Holesky network with your MetaMask wallet address
-        let address = new ExternalAddress(Coinbase.networks.EthereumHolesky, "0x594e8de27f320A7C51166389Db34F9D3c7ef0492");
+        let address = new ExternalAddress(Coinbase.networks.EthereumHolesky, "YOUR_WALLET_ADDRESS_HERE");
         
         // Get the stakeable balance of Ethereum
         let stakeableBalance = await address.stakeableBalance(Coinbase.assets.Eth, StakeOptionsMode.NATIVE);
